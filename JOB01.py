7379b882dd1be4890530780835e650c92edf182c
OLIVIER PORTAL <olivier.portal@laplateforme.io>
2024-11-26 13:50:53 +0100

    JOB01 et 02 OK

---
+++ JOB01.py
@@ -0,0 +1,7 @@
+valeur1 = input("Entrez une valeur : ")
+valeur2 = input("Entrez une autre valeur : ")
+
+if valeur1 == valeur2:
+    print(f"{valeur1} est égale à {valeur2}")
+else:
+    print(f"les deux valeurs ne sont pas égales")
---
+++ JOB02.py
@@ -0,0 +1,6 @@
+age = int(input("Quel est ton âge ? "))
+
+if age >= 18:
+    print("Tu peux voter")
+else:
+    print("Tu ne peux pas voter")
