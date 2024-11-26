a631dc109b5299b2f25b28a3802d913d3ac8a1b2
OLIVIER PORTAL <olivier.portal@laplateforme.io>
2024-11-26 17:03:34 +0100

    JOB05 Ok

--- JOB05.py
+++ JOB05.py
@@ -1,26 +1,8 @@
-# Création d'un fonction qui renvoie un boolean
-
-def is_prime(nb):
-    # Un nombre premier ne peut être égale à 0 ou 1
-    if nb == 0 or nb == 1:
-        return False
-    # Les nombres premiers sont divisable par leurs racine² (ou avoir une puissance de 0.5)
-    else:
-        diviseur_a_tester = 2
-        racine = nb**0.5
-        while diviseur_a_tester <= racine:
-            if nb % diviseur_a_tester == 0:
-                return False
-            diviseur_a_tester += 1
-        return True
-    
-    
-nombre = 0
-nombre2 = 0
-
-for nombre in range(1000):
-    nombre2 += 1
-    if is_prime(nombre) == True:
-        nombre += 1
-        print(nombre2)
+for n in range(1000):
+   if n > 1:
+       for i in range(2,n):
+           if (n % i) == 0:
+               break
+       else:
+           print(n)
         
