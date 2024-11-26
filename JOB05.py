00c64aa4ca43dd0f1fa88783ae0f291e4a461cd7
OLIVIER PORTAL <olivier.portal@laplateforme.io>
2024-11-26 16:59:55 +0100

    test JOB05

--- JOB05.py
+++ JOB05.py
@@ -1,11 +1,26 @@
-i = 2
-number = int()
-number <= 1000
-number > i
+# Création d'un fonction qui renvoie un boolean
 
-for i in range(2, 1000):
-    i += 1
-    if int(i**0.5)== 0:
-        print(i)
-        i += 1
+def is_prime(nb):
+    # Un nombre premier ne peut être égale à 0 ou 1
+    if nb == 0 or nb == 1:
+        return False
+    # Les nombres premiers sont divisable par leurs racine² (ou avoir une puissance de 0.5)
+    else:
+        diviseur_a_tester = 2
+        racine = nb**0.5
+        while diviseur_a_tester <= racine:
+            if nb % diviseur_a_tester == 0:
+                return False
+            diviseur_a_tester += 1
+        return True
+    
+    
+nombre = 0
+nombre2 = 0
+
+for nombre in range(1000):
+    nombre2 += 1
+    if is_prime(nombre) == True:
+        nombre += 1
+        print(nombre2)
         
