c5c2c7cb1d47ea5a1358825e5507c431f04053ba
OLIVIER PORTAL <olivier.portal@laplateforme.io>
2024-11-26 16:28:01 +0100

    JOB04 OK and JOB05 coming

---
+++ JOB04.py
@@ -0,0 +1,11 @@
+i = 0
+
+for i in range(101):
+    if i % 3 == 0 and i % 5 != 0:
+        print("fizz")
+    elif i % 5 == 0 and i % 3!= 0:
+        print("buzz")
+    elif i % 3 == 0 and i % 5 ==0:
+        print("fizzbuzz")
+    else:
+        print(i)
---
+++ JOB05.py
@@ -0,0 +1,11 @@
+i = 2
+number = int()
+number <= 1000
+number > i
+
+for i in range(2, 1000):
+    i += 1
+    if int(i**0.5)== 0:
+        print(i)
+        i += 1
+        
