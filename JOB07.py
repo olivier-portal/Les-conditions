4bb5b23e9bc328c94c823f47f8ec8ddd569da307
OLIVIER PORTAL <olivier.portal@laplateforme.io>
2024-11-27 09:04:49 +0100

    test2 JOB07

--- JOB07.py
+++ JOB07.py
@@ -1,10 +1,8 @@
+word = "abcdefghijklmnopqrstuvwxyz"
+x = ""
 rows = 10
-ascii_value = 97
-
 for i in range(rows):
-    for j in range(i+1):
-        alphabet = chr(ascii_value)
-        for k in range(j+1):
-            ascii_value += 1
-            print(alphabet, end='')
+    for i in word:
+        x += i
+        print(x)
     print()
